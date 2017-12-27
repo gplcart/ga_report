e08dc2d2a6304eebe4b63c64619b17cc02564f41
GplCart <gplcart.software@gmail.com>
2017-12-27 14:03:55 +0200

    Rename main module class to Module

--- Module.php
+++ Module.php
@@ -9,13 +9,13 @@
 
 namespace gplcart\modules\ga_report;
 
-use gplcart\core\Module,
-    gplcart\core\Container;
+use gplcart\core\Container,
+    gplcart\core\Module as CoreModule;
 
 /**
  * Main class for Google Analytics Report module
  */
-class GaReport
+class Module
 {
 
     /**
@@ -25,9 +25,9 @@ class GaReport
     protected $module;
 
     /**
-     * @param Module $module
+     * @param CoreModule $module
      */
-    public function __construct(Module $module)
+    public function __construct(CoreModule $module)
     {
         $this->module = $module;
     }
